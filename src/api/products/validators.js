0eedbbec4ff64df44218bebfa1c8cf23a6bf88f2
Francisco-Fortes <fortesfco@gmail.com>
2023-01-06 18:47:29 +0100

    validators added

---
+++ src/api/products/validators.js
@@ -0,0 +1,53 @@
+import { checkSchema } from "express-validator";
+import createHttpError from "http-errors";
+
+const productSchema = {
+  name: {
+    in: ["body"],
+    isString: {
+      errorMessage: "Name is a mandatory field, it has to be a string",
+    },
+  },
+
+  description: {
+    in: ["body"],
+    isString: {
+      errorMessage: "Description is a mandatory field, it has to be a string",
+    },
+  },
+  brand: {
+    in: ["body"],
+    isString: {
+      errorMessage: "Brand is a mandatory field, it has to be a string",
+    },
+  },
+  price: {
+    in: ["body"],
+    isNumeric: {
+      errorMessage: "Price is a mandatory field, it has to be a number",
+    },
+  },
+  category: {
+    in: ["body"],
+    isString: {
+      errorMessage: "Category is a mandatory field, it has to be a string",
+    },
+  },
+};
+
+export const checksProductsSchema = checkSchema(productSchema);
+
+export const checkValidationResult = (req, res, next) => {
+  const errors = validationResult(req);
+  console.log(errors.array());
+  //If the error list is not empty...
+  if (!errors.isEmpty()) {
+    next(
+      createHttpError(400, "Errors during product validation", {
+        errorList: errors.array(),
+      })
+    );
+  } else {
+    next();
+  }
+};
